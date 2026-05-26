8d280244154290ddd428ee785b716d1c636366bd
drevlados <drevlados@yandex.com>
2026-05-26 10:41:17 +0300

    Delete App_for_isetup directory

--- App_for_isetup/lab4_bat.bat
+++
@@ -1,5 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio
-9.0\Common7\IDE\VCExpress.exe" project_test.sln /build Debug
-"C:\Program Files\doxygen\bin\doxygen.exe" myConfig
-"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" build.iss
-pause
